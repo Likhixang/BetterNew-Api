30a98061eaad4dcca85796d3e2b99f60dbb383c5
Likhixang <Likhixang@users.noreply.github.com>
2026-08-06 10:41:55 +0800

    refactor(keys): Rename Allowed Channels to Channel Limits, move before Model Mapping

--- web/src/features/keys/components/api-keys-mutate-drawer.tsx
+++ web/src/features/keys/components/api-keys-mutate-drawer.tsx
@@ -737,31 +737,6 @@ export function ApiKeysMutateDrawer({
 
                     <FormField
                       control={form.control}
-                      name='model_mapping'
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>{t('Model Mapping')}</FormLabel>
-                          <FormControl>
-                            <ModelMappingEditor
-                              value={field.value || ''}
-                              onChange={field.onChange}
-                              disabled={isSubmitting}
-                              sourceModelOptions={models}
-                              targetModelOptions={models}
-                            />
-                          </FormControl>
-                          <FormDescription>
-                            {t(
-                              'Redirect a requested model to another model for this key.'
-                            )}
-                          </FormDescription>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-
-                    <FormField
-                      control={form.control}
                       name='channel_limits'
                       render={({ field }) => (
                         <FormItem>
@@ -779,6 +754,31 @@ export function ApiKeysMutateDrawer({
                           <FormDescription>
                             {t(
                               'Limit which channels can be used with this key. Group rules still apply on top of this whitelist.'
+                            )}
+                          </FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name='model_mapping'
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>{t('Model Mapping')}</FormLabel>
+                          <FormControl>
+                            <ModelMappingEditor
+                              value={field.value || ''}
+                              onChange={field.onChange}
+                              disabled={isSubmitting}
+                              sourceModelOptions={models}
+                              targetModelOptions={models}
+                            />
+                          </FormControl>
+                          <FormDescription>
+                            {t(
+                              'Redirect a requested model to another model for this key.'
                             )}
                           </FormDescription>
                           <FormMessage />
